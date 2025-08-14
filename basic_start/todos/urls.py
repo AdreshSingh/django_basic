089013ec3af5404104bcf5d941b9f62ac3605889
Devank Singh <adreshsingh271527@gmail.com>
2025-08-14 21:24:09 +0530

    added urls such as Create, Read, Delete views

--- basic_start/todos/urls.py
+++ basic_start/todos/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 # ! adding views/pages
 
-from .views import home,addtodos
+from .views import home,addtodos,detail,deletetodo
 
 urlpatterns=[
     path('',home,name="homepage"),
-    path('add',addtodos,name="New Todo")
+    path('add',addtodos,name="New Todo"),
+    path("details/<int:id>/",detail,name="details"),
+    path("delete/<int:id>/",deletetodo,name="deletetodo")
 ]
