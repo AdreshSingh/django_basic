d3a671fd235c656d7a49ca34d8d4934ece22952d
Devank Singh <adreshsingh271527@gmail.com>
2025-08-14 22:22:22 +0530

    added url for update operation

--- basic_start/todos/urls.py
+++ basic_start/todos/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 # ! adding views/pages
 
-from .views import home,addtodos,detail,deletetodo
+from .views import home,addtodos,detail,deletetodo,updatetodo
 
 urlpatterns=[
     path('',home,name="homepage"),
     path('add',addtodos,name="New Todo"),
     path("details/<int:id>/",detail,name="details"),
-    path("delete/<int:id>/",deletetodo,name="deletetodo")
+    path("delete/<int:id>/",deletetodo,name="deletetodo"),
+    path("update/<int:id>",updatetodo,name="updatetodo")
 ]
